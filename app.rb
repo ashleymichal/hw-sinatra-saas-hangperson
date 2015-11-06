6868c9abdaceea84e2046ad5c46dbeab0638180f
Ashley Michal <ashleymichal@gmail.com>
2015-11-06 17:25:26 +0000

    rescue invalid letter

--- app.rb
+++ app.rb
@@ -40,7 +40,11 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess(letter) || flash[:message] = "You have already used that letter."
+    begin
+      @game.guess(letter) || flash[:message] = "You have already used that letter."
+    rescue
+      flash[:message] = "Invalid"
+    end
     case @game.check_win_or_lose
     when :win
       redirect '/win'
